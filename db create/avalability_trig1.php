6c908cc0912cf6fe6eea06ac3f3829473d543e7a
Puneet <34083630+Puneet-Jain-18@users.noreply.github.com>
2020-01-09 12:28:27 +0530

    Updated to php form txt

---
+++ db create/avalability_trig1.php
@@ -0,0 +1,50 @@
+<?php
+$host="localhost";
+$user="root";
+$pass="";
+$conn=new mysqli($host,$user,$pass);
+if($conn->connect_error)
+{
+    die ("Unsucessful");
+}
+echo "Connection made";
+$query=$query="
+create trigger avalability2
+	after update on cart
+	for each row 	
+	begin
+	if new.final='y' then
+	update pet set avalability='n'
+	where p_id=new.pet_id;
+	
+	update animals set 
+	quantity =quantity-1 where
+	aid=(select aid from pet where p_id=new.pet_id);
+  end if;
+end;
+";
+
+if($conn->query($query)==TRUE)
+{
+    echo ("Trigger 1 Executed Sucessfully");
+}
+else
+{
+    echo("Error in the Trigger1");
+}
+
+$query="
+create trigger avalability2
+	after update on cart
+	for each row 	
+	begin
+	if new.final='y' then
+	update pet set avalability='n'
+	where p_id=new.pet_id;
+	
+	update animals set 
+	quantity =quantity-1 where
+	aid=(select aid from pet where p_id=new.pet_id);
+  end if;
+end;
+";
