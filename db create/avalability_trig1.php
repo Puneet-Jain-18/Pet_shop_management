5a3ad6ff04748890c73e320491e4fdb1d2ff86f3
Puneet <34083630+Puneet-Jain-18@users.noreply.github.com>
2020-01-09 12:31:45 +0530

    Removed duplicate query

--- db create/avalability_trig1.php
+++ db create/avalability_trig1.php
@@ -8,8 +8,7 @@ if($conn->connect_error)
     die ("Unsucessful");
 }
 echo "Connection made";
-$query="
-create trigger avalability2
+$query="create trigger avalability2
 	after update on cart
 	for each row 	
 	begin
@@ -32,19 +31,3 @@ else
 {
     echo("Error in the Trigger1");
 }
-
-$query="
-create trigger avalability2
-	after update on cart
-	for each row 	
-	begin
-	if new.final='y' then
-	update pet set avalability='n'
-	where p_id=new.pet_id;
-	
-	update animals set 
-	quantity =quantity-1 where
-	aid=(select aid from pet where p_id=new.pet_id);
-  end if;
-end;
-";
